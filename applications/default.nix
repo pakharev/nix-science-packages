b523f5d49294014c2b2a61bc157a8df34bf4c037
Aleksei Pakharev <alexey.pakharev@gmail.com>
2023-10-26 13:19:28 -0400

    fix rstudio call

--- applications/default.nix
+++ applications/default.nix
@@ -1,3 +1,6 @@
 final: prev: {
-  rstudio = final.callPackage ./rstudio {};
+  rstudio = prev.libsForQt5.callPackage ./rstudio {
+    inherit (prev.yarn2nix-moretea) fixup_yarn_lock;
+    jdk = prev.jdk8;
+    patchShebangs = prev.callPackage ./rstudio/patch-shebangs.nix {};
 }

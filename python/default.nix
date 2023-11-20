3b0c290d4b98011f4f4187112e90b8da9e9f1f59
Aleksei Pakharev <alexey.pakharev@gmail.com>
2023-11-19 21:22:02 -0500

    add umap-learn to the package list

--- python/default.nix
+++ python/default.nix
@@ -3,17 +3,18 @@ final: prev: {
     packageOverrides = (self: super: {
       anndata = self.callPackage ./anndata {};
       array-api-compat = self.callPackage ./array-api-compat {};
-      numpy-groupies = self.callPackage ./numpy-groupies {};
-      get-annotations = self.callPackage ./get-annotations {};
-      session-info = self.callPackage ./session-info {};
-      mudata = self.callPackage ./mudata {};
-      loompy = self.callPackage ./loompy {};
-      scanpy = self.callPackage ./scanpy {};
-      scvi-tools = self.callPackage ./scvi-tools {};
-      scvelo = self.callPackage ./scvelo {};
-      unitvelo = self.callPackage ./unitvelo {};
-      iprogress = self.callPackage ./iprogress {};
       flax = self.callPackage ./flax {};
+      get-annotations = self.callPackage ./get-annotations {};
+      iprogress = self.callPackage ./iprogress {};
+      loompy = self.callPackage ./loompy {};
+      mudata = self.callPackage ./mudata {};
+      numpy-groupies = self.callPackage ./numpy-groupies {};
+      scanpy = self.callPackage ./scanpy {};
+      scvelo = self.callPackage ./scvelo {};
+      scvi-tools = self.callPackage ./scvi-tools {};
+      session-info = self.callPackage ./session-info {};
+      umap-learn = self.callPackage ./umap-learn {};
+      unitvelo = self.callPackage ./unitvelo {};
 
       tensorflow = self.tensorflow-bin;
     });
